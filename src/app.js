5c2f1afe903c69d2b6295eaacbce7dae97e08dcd
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-06 19:07:25 +0000

    Add button functionality

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+
+const generateRandomCard = () => {
   // Rank and suit data
   const suits = ["diamond", "heart", "spade", "club"];
   const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  
+
   // Select a random array element for suit and rank
   const randomPropertySelector = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -24,9 +24,13 @@ window.onload = function() {
     <div id="rank" class="suit-top text-center">${randomRank}</div> 
     <div class="suit suit-bottom text-end ${randomSuit}"></div> 
   `
+}
 
+window.onload = function () {
+  generateRandomCard();
   // Display card once it's generated
   document.querySelector('.container').removeAttribute('hidden');
 };
 
-
+// Button functionality to generate a new random card
+document.querySelector('button').addEventListener('click', generateRandomCard);

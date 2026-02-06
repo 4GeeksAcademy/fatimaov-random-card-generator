4c474b20cbd3b5c00295569c3ba63058b3da5b73
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-06 14:20:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // Rank and suit data
+  const suits = ["♦", "♥", "♠", "♣"];
+  const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  // DOM elements
+  const rankElement = document.getElementById('rank');
+  const suitElements = document.querySelectorAll('.suit');
+  
+  // Select a random array element for suit and rank
+  const randomPropertySelector = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  const randomSuit = randomPropertySelector(suits);
+  const randomRank = randomPropertySelector(ranks);
+  rankElement.innerHTML = randomRank;
+  suitElements.forEach((suitElement) => {
+    if(randomSuit === "♥" || randomSuit === "♦") {
+      suitElement.style.color = 'var(--bs-danger)';
+    }
+    suitElement.innerHTML = randomSuit;
+  })
+
+  // Display card once it's generated
+  document.querySelector('.container').removeAttribute('hidden');
 };
+
+
